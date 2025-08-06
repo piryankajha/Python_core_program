ba6b9c0de790cf30f3586b588bad5ac16f840003
piryankajha <piryankajha06@gmail.com>
2025-08-07 00:28:21 +0530

    If, elif, else: conditions

---
+++ Python_Conditional_Statements.py
@@ -0,0 +1,61 @@
+# Python_Conditional_Statements.py
+"""
+Topic: Conditional Statements in Python
+Author : Piryanka Jha   
+"""
+
+# ---------------------------
+# What are Conditional Statements?
+# ---------------------------
+# Conditional statements are used to perform different actions based on different conditions.
+# Python uses if, elif, and else keywords for decision making.
+
+# ---------------------------
+# Basic if Statement
+# ---------------------------
+age = 23
+if age >= 18:
+    print("" eligible to vote.")
+
+# ---------------------------
+# if-else Statement
+# ---------------------------
+num = 9
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+# ---------------------------
+# if-elif-else Ladder
+# ---------------------------
+marks = 78
+if marks >= 985:
+    print("Grade: A")
+elif marks >= 75:
+    print("Grade: B")
+elif marks >= 60:
+    print("Grade: C")
+else:
+    print("Grade: D or below")
+
+# ---------------------------
+# Nested if Statements
+# ---------------------------
+x = 10
+y = 20
+if x > 7:
+    if y > 18:
+        print("Both conditions are True")
+
+# ---------------------------
+# Using Logical Operators with Conditions
+# ---------------------------
+username = "Piryanka"
+password = "6655"
+if username == "admin" and password == "1234":
+    print("Login successful")
+else:
+    print("Login failed")
+
+#---------------------------------------------------------------
