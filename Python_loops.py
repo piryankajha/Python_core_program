78a4ac29281cbe4dad246b3164a7db46cf3c3f7e
Piryanka Jha <piryankajha06@gmail.com>
2025-08-19 23:55:29 +0530

    Update Python_loops.py

--- Python_loops.py
+++ Python_loops.py
@@ -153,6 +153,13 @@ for i in range(1,21):
     if i%2!=0:
         print("Odd no : ",i)
 
+'''to print all number which is divisible by 3 and 2'''
+
+for i in range (10,41):
+    if i%3==0 and i%2==0:
+        print(i)
+
+
 
 
 
