223d18f662e45d546f3c9ab5b7f130338386b485
Piryanka Jha <piryankajha06@gmail.com>
2025-09-12 23:17:45 +0530

    Update Python_loops.py

--- Python_loops.py
+++ Python_loops.py
@@ -246,11 +246,14 @@ for i in range(0,21):
       continue
     print(i)
 
+'''Write a program using a for loop to print numbers from 1 to 15,
+     but skip numbers divisible by 3 '''
 
 for i in range(1,15):
     if i%3==0:
         continue
     print(i)
+
 
 
 
