04dfc0d40d236d22c659ebb3b16daa3810aa249c
Piryanka Jha <piryankajha06@gmail.com>
2025-09-19 23:00:47 +0530

    Update Python_loops.py

--- Python_loops.py
+++ Python_loops.py
@@ -300,6 +300,9 @@ for i in range(1,21):
      x=i**3
      print(x)
 
+'''Write a Python program that takes an integer input from the user and 
+       prints the squares of all numbers from 1 to that number'''
+
 
 
 
