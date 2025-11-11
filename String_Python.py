c81516fb01987ab66852eae50317c34f94fef092
Piryanka Jha <piryankajha06@gmail.com>
2025-11-11 23:28:11 +0530

    Update String_Python.py

--- String_Python.py
+++ String_Python.py
@@ -5,7 +5,7 @@ Author: Priyanka Jha
 
 """
  
-# -------------------------
+# ------------------------- 
 # 1. What is a String?
 # -------------------------
 # A string is a sequence of characters enclosed in single quotes (' '), double quotes (" "), or triple quotes (''' ''' / """ """).
@@ -395,6 +395,7 @@ y=x[0].upper()
 z=x[-1].upper()
 a=x[1:-2].lower()
 print(y+a+z)
+
 
 
 
