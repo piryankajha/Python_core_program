4fcbed9faf9037ced6ab8d8a9783b72ffb4a900e
Piryanka Jha <piryankajha06@gmail.com>
2026-02-13 23:58:36 +0530

    Update Function.py

--- Function.py
+++ Function.py
@@ -4,7 +4,7 @@
 '''
 # ---------------------------  
 # What is a Function?    
-# -------------------------  
+# -------------------------   
 # A function is a block of reusable code that performs a specific task.
 # Functions help in code reusability and make programs easier to manage.
 
@@ -136,6 +136,7 @@ def cir(r):
     print("Area of circle : ",r)
 radiaus=float(input("enter radius:_ "))
 cir(radiaus)
+
 
 
 
