797cac3473c60cc35e4da94e07d239b6b5a946f3
Piryanka Jha <piryankajha06@gmail.com>
2026-05-04 21:41:14 +0530

    Update Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -10,7 +10,7 @@
 2->  dict are ordered .
 3->  dict are mutable or changable ._
 4->  dict are not indexed .   
-5->  dict not allow duplicate keys .
+5->  dict not allow duplicate keys.
 6-.  it also support multiple typpe of Data.  
 _________________________________________________________________________________________________________
 
