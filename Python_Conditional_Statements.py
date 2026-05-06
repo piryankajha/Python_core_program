e8fbc59d5dd7d0656d81a6e6f49fdff2eaa1ba22
Piryanka Jha <piryankajha06@gmail.com>
2026-05-06 22:16:26 +0530

    Update Python_Conditional_Statements.py

--- Python_Conditional_Statements.py
+++ Python_Conditional_Statements.py
@@ -5,7 +5,7 @@ Author : Piryanka Jha
 """
   
 # -------------------------------
-# What are Conditional Statements?
+# What are Conditional Statements?.
 # ------------------------------
 # Conditional statements are used to perform different actions based on different conditions.
 # Python uses if, elif, and else keywords for decision making.
