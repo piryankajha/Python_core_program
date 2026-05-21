462696102def6a05871aaec873072ec7ff26321d
Piryanka Jha <piryankajha06@gmail.com>
2026-05-21 22:59:53 +0530

    Refactor and enhance function definitions in Function.py

--- Function.py
+++ Function.py
@@ -10,7 +10,7 @@
 # def function_name(parameters):
 #     """docstring (optional)"""
 #     # block of code
-#     return value (optional)                              '''
+#     return value (optional)                       '''
 
 
 # ---------------------------
